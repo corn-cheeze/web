49edba50663ec5621a9aa4e483ba55a64887be83
kimsohyeon <aotoyae@gmail.com>
2024-08-28 21:14:44 +0900

    feat: home 에 로그인/회원가입 페이지 이동 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="">
       <h1>home</h1>
+      <Link href="login">➡️ login</Link>
+      <Link href="join">➡️ join</Link>
     </main>
   );
 }
