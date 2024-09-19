e46f9f64c1ec34d60e622db21480c6a0b12c365f
kimsohyeon <aotoyae@gmail.com>
2024-09-20 04:04:46 +0900

    setting: tailwind postcss autoprefixer 설치

--- src/compotents/Header.tsx
+++ src/compotents/Header.tsx
@@ -4,16 +4,16 @@ const Header = () => {
   return (
     <div className="flex flex-col gap-4">
       <section className="flex justify-between text-pointColor1">
-        <h1 className="font-black">CORNCHEEZE</h1>
+        <h1 className="bg-red-900 text-9xl font-black">CORNCHEEZE</h1>
         <div>
           <Link href="login">로그인</Link>
           <span>/</span>
           <Link href="join">회원가입</Link>
         </div>
       </section>
-      <nav className="h-[30px] w-full bg-mainColor px-2">
-        <ul className="flex h-full">
-          <li className="text-white">자유게시판</li>
+      <nav className="bg-mainColor text-white">
+        <ul className="">
+          <li className="text-9xl">자유게시판</li>
         </ul>
       </nav>
     </div>
