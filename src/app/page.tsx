402e78f3727b4f6d93130484a51433d65d0a5ac2
kimsohyeon <aotoyae@gmail.com>
2024-09-19 10:48:20 +0900

    feat: nav 구성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import Link from "next/link";
-
 export default function Home() {
-  return (
-    <main className="">
-      <h1>home</h1>
-      <Link href="login">➡️ login</Link>
-      <Link href="join">➡️ join</Link>
-    </main>
-  );
+  return <main className=""></main>;
 }
--- src/compotents/Header.tsx
+++ src/compotents/Header.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <section>
+        <h1>home</h1>
+        <div>
+          <Link href="login">➡️ login</Link>
+          <Link href="join">➡️ join</Link>
+        </div>
+      </section>
+      <nav>
+        <ul>
+          <li>자유게시판</li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
