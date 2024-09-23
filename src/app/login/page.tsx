33a8d883260551ffe4fcf2b056219c1185544b3b
kimsohyeon <aotoyae@gmail.com>
2024-09-23 20:59:03 +0900

    style: 로그인/회원가입 form border 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -33,8 +33,8 @@ const page = () => {
 
   return (
     <main className="flex h-screen flex-col items-center justify-center">
-      <div className="flex w-[420px] flex-col justify-center gap-12 rounded-lg bg-white px-6 py-12 drop-shadow-xl">
-        <h1 className="text-pointColor1 text-center text-3xl font-black">
+      <div className="flex w-[420px] flex-col justify-center gap-12 rounded-lg border border-solid border-rightGray px-6 py-12">
+        <h1 className="text-center text-3xl font-black text-pointColor1">
           CORNCHEEZE
         </h1>
         <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
@@ -44,29 +44,29 @@ const page = () => {
               placeholder="아이디"
               ref={idRef}
               onBlur={handleBlur}
-              className="border-rightGray placeholder-rightGray focus:outline-mainColor h-14 rounded-sm border border-solid pl-3"
+              className="h-14 rounded-sm border border-solid border-rightGray pl-3 placeholder-rightGray focus:outline-mainColor"
             />
             <input
               type="text"
               placeholder="비밀번호"
-              className="border-rightGray placeholder-rightGray focus:outline-mainColor h-14 rounded-sm border border-solid pl-3"
+              className="h-14 rounded-sm border border-solid border-rightGray pl-3 placeholder-rightGray focus:outline-mainColor"
             />
           </div>
-          <label className="text-gray flex items-center gap-2 pb-2">
+          <label className="flex items-center gap-2 pb-2 text-gray">
             <input
               type="checkbox"
               checked={stayLogin}
               onChange={handleStayLogin}
-              className="border-gray checked:bg-mainColor h-5 w-5 appearance-none rounded-full border border-solid checked:border-none"
+              className="h-5 w-5 appearance-none rounded-full border border-solid border-gray checked:border-none checked:bg-mainColor"
             />
             로그인 상태 유지
           </label>
           {id === "hi" && <p>Warning</p>}
-          <button className="bg-mainColor h-12 rounded-sm font-bold tracking-wider text-white">
+          <button className="h-12 rounded-sm bg-mainColor font-bold tracking-wider text-white">
             로그인
           </button>
         </form>
-        <section className="text-gray flex justify-between">
+        <section className="flex justify-between text-gray">
           <Link href="join">회원가입</Link>
           <div className="flex gap-4">
             <button>아이디 찾기</button>
