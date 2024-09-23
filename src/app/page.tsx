143f2076f441f34eda64eadab96b2ec16ff775b5
kimsohyeon <aotoyae@gmail.com>
2024-09-24 01:20:48 +0900

    feat: 포스트맨 GET user 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { MOCK_URL } from "@/server";
+
 export default function Home() {
-  return <main className=""></main>;
+  const [userId, setUserId] = useState(null);
+
+  const load = async () => {
+    try {
+      const response = await axios.get(MOCK_URL);
+      setUserId(response.data[0].id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    load();
+  }, []);
+  console.log(userId);
+
+  return (
+    <main className="">
+      <h1>userTest</h1>
+      {userId && <p>id: {userId}</p>}
+    </main>
+  );
 }
